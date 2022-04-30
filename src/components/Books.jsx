c12daeae4ef984a3dcea79ad948969d1766e9e8c
Mario Blanco Dominguez <mario10_bd@hotmail.com>
2022-04-30 09:30:18 +0200

    refactor from js to jsx

---
+++ src/components/Books.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import SearchBooks from "./SearchBooks";
+
+export class Books extends Component {
+  render() {
+    return (
+      <div>
+        <div className=""></div>
+        <SearchBooks> </SearchBooks>
+      </div>
+    );
+  }
+}
+
+export default Books;
